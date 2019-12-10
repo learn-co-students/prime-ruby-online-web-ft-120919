24bf887a40851040923ad71ffa4a9d68b08f9f53
allison_johnson <allison.cuttler@gmail.com>
2019-12-10 13:45:22 -0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  #num = num.abs
+  if num <= 1
+    false
+  elsif num == 2
+    true
+  else
+    i = 2
+    while i < num
+        return false if num % i == 0
+        i += 1
+    end #while
+    true
+  end #if
+end 
