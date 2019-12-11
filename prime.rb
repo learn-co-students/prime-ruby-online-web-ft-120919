e38a96935c54690f50f94af935637106f923044f
Benjamin Small <ben20262@aim.com>
2019-12-11 00:26:21 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  if num > 2
+    array = (2...num).to_a
+    array.each do |x|
+      return false if num % x == 0
+    end
+    return true
+  elsif num < 2
+    return false
+  else
+    return true
+  end
+end
