21bcd1889569259018bbcb6373d4d3386de74c0f
Tammy Lam <lamtammy1515@gmail.com>
2019-12-12 22:05:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(max)
+  low = 2
+  if max > 1
+    range = (low..max-1).to_a
+    range.none? do |num_to_test|
+      max % num_to_test == 0
+    end
+  else
+    false
+  end
+end
