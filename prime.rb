ac3f9aab248055079899321bede12a32214721fd
nwscala <nwscala@gmail.com>
2019-12-12 10:40:37 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+    if number <= 1 
+        return false
+    elsif number == 2
+        return true
+    else 
+        number_half = (number/2.0).ceil()
+        check_array = (2..number_half).to_a
+        check_array.each do |check_num|
+            return false if number % check_num == 0
+        end
+        return true
+    end
+end
