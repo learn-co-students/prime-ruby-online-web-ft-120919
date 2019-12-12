e7fb0c16b3e060a10adb6febdd6aa840509e5585
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-12 21:35:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  start = 2
+  if number > 1
+    create_range = (start..number-1).to_a
+    create_range.none? do |num_to_test|
+    number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
