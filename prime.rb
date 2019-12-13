69a473cc66564543fdf00f0e51f51f28751c607c
mrarthurwhite <mister.arthur.white@gmail.com>
2019-12-13 15:56:58 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(n)
+    # Corner case 
+    if (n <= 1)  
+        return false
+    end
+    #Check from 2 to n-1 
+    i = 2
+
+    while (i<n)
+            if (n%i == 0) 
+                return false 
+            end
+            i+=1 
+    end
+
+    return true
+end
+
+puts "2 #{prime?(2)}"
+puts "-1 #{prime?(-1)}"
