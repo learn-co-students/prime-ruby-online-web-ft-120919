00df6f70a4ad825a9a725696ca72fea1cdacd8f4
Devon James Wright <devonjamess.wright@gmail.com>
2019-12-14 13:16:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require "prime"
+
+def prime?(num)
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
+  end
+  if num <= -1
+    puts "Be sure to account for negative numbers!"
+  end
+  true
+end
+  yield
