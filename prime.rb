12b3f793903c6a4b9c02a6727dfb84e2b193d42e
github username <github email address>
2019-12-13 21:49:43 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+# require 'pry'
+#     # binding.pry
+
+def prime?(n) 
+    return false if n < 2
+    return true if n == 2
+    (2..Math.sqrt(n).ceil).each do |x|
+     return false if n % x == 0
+        end
+    true
+end
