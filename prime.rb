ea2f2309d4b433db2e0579c830aa1f13752df717
Joshua Sanders <ninjosh2@gmail.com>
2019-12-17 06:28:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  start = 2
+  if num > 1
+    range = (start..num-1).to_a
+    range.none? do |numberz| 
+      
+      num % numberz == 0
+    end
+  else
+    
+    false
+  end
+  
+end
