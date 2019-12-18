003355aeb9649a0c433f62459e569c2d1277f571
Loren Cipriano <loren25cipriano@gmail.com>
2019-12-18 01:36:19 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime? (num)# Add  code here!
+  if num < 0 or num == 0 or num == 1 
+    return false
+  else 
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor !=0 
+    end
+  end
+end
+puts prime?(105557)
